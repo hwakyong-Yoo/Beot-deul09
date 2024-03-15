185d35bdf63bf7dfca0b590e6e945e87735baffa
hwakyong YOO <hwakyongyoo@gmail.com>
2024-03-15 19:50:27 +0900

    [BE] fix: age int로 수정

--- server/src/main/java/com/example/ewhaproject/entity/User.java
+++ server/src/main/java/com/example/ewhaproject/entity/User.java
@@ -29,7 +29,7 @@ public class User {
     private String name;
 
     @Column
-    private int age;
+    private Integer age;
 
     public void updateFromDto(UserDto userDto) { //회원 정보 수정
         this.setPassword(userDto.getPassword());
